06c108445ca704d1940a26a573c1c696ee0be80d
Gabriel Serna <109190797+gabserna@users.noreply.github.com>
2023-04-05 23:09:21 -0600

    functions corrected to obtain & render from api

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
+const select = document.getElementById('course-select');
+let holes;
+
 async function loadCourse() {
-  const response = await fetch('courses.json');     // https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json   --url principal--
+  const response = await fetch('https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json');
   const data = await response.json();
-  return data.data.course;
+  data.map(row=>{
+    const option = document.createElement('option')
+    option.value = row.id
+    option.text = row.name
+    option.setAttribute('data-url', row.url)
+    select.appendChild(option)
+
+  })
 }
 
-const select = document.getElementById('course-select');
+select.addEventListener('change', courseSelect);
 const courseTitle = document.getElementById('course-title');
 let url;
 
-select.addEventListener('change', courseSelect);
-
 async function courseSelect() {
+  let opciones = this.getElementsByTagName("option")
   const courseId = select.value;
-  const courses = await loadCourse();
-  const selectedCourse = courses.find(course => course.id.toString() === courseId);
-  courseTitle.innerHTML = selectedCourse.courseName;
-  url = selectedCourse.url;
-  
+
+  courseTitle.innerHTML = opciones[this.selectedIndex].innerHTML
+  url = opciones[this.selectedIndex].getAttribute('data-url');
+  console.log(url)
   try {
       const response = await fetch(url);
       const data = await response.json();
@@ -26,55 +34,57 @@ async function courseSelect() {
   }
 }
 
-async function getTee(courseData) {
-  let myTeeSelection = `<select class="form-control" id="tee-select" onchange="teeSelect(this.id)"><option value="initial">&#171; SELECT TEE COLOR &#187;</option>`;
 
-  const teeBoxes = courseData.holes[0].teeBoxes;
-  teeBoxes.forEach(tee => {
-    if (tee.teeColorType !== null) {
-      myTeeSelection += `<option value="${tee.teeColorType}">${tee.teeColorType}</option>`;
-    }
-  });
+async function getTee(courseData) {
+  console.log(courseData)
+  let myTeeSelection = `<select class="form-control" id="tee-select" onchange="teeSelect(this)"><option value="initial">&#171; SELECT TEE COLOR &#187;</option>`;
+  
+  holes = courseData.holes
+  let addedTeeColors = [];
+  
+  holes.map(row => {
+    const teeBoxes = row.teeBoxes
+    teeBoxes.forEach(tee => {
+      if (tee.teeColorType !== null && !addedTeeColors.includes(tee.teeColorType)) {
+        myTeeSelection += `<option value="${tee.teeColorType}">${tee.teeColorType}</option>`;
+        addedTeeColors.push(tee.teeColorType);
+      }
+    });
+  })
+  
   myTeeSelection += `</select>`;
   document.getElementById('tee-title').innerHTML = myTeeSelection;
 }
 
 
-async function teeSelect(clickedId) {
+
+async function teeSelect(e) {
+  let color = e.value
   const teeTitle = document.getElementById('tee-title');
-  let color = document.getElementById(clickedId).value
   teeTitle.innerHTML = "Tee Box: " + color;
 
   let course;
+  let teeType = []
   try {
-    const response = await fetch('https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course11819.json');  // debe venir de courses.json o del url principal
-    course = await response.json();
-    if (!course || !course.holes || !Array.isArray(course.holes)) {
-      console.log(course);
-      throw new Error('nothing to show...!!');
-    }
+    let bxs =[];
+    holes.map(hol=>{
+      tBoxes = hol.teeBoxes
+      let queryBoxes = tBoxes.find(boxes=> boxes.teeColorType == color)
+      bxs.push(queryBoxes)
+    })
+    console.log(bxs)
+    bxs.forEach(teeBox=>{
+      teeType.push({ id: teeBox.courseHoleId, name: '', teeBoxYards: teeBox.yards, teeBoxPar: teeBox.par, teeBoxHcp: teeBox.hcp,
+          });
+    })
   } catch (error) {
     console.log(`ERROR: ${error}`);
     return;
   }
-
-  let teeType = []
-  course.holes.forEach(hole => {
-    if (hole && hole.teeBoxes) {
-      hole.teeBoxes.forEach(teeBox => {
-        if (teeBox && teeBox.teeColorType === color) {
-          teeType.push({ id: teeBox.courseHoleId, name: '', teeBoxYards: teeBox.yards, teeBoxPar: teeBox.par, teeBoxHcp: teeBox.hcp,
-          });
-        }
-      });
-    }
-  });
-
+  console.log(teeType)
   showScoreCard(teeType);
 }
 
-
-// version corregida!!!!!!!
 async function showScoreCard(teeType) {
   let body = "";
   let parBody = "";
@@ -93,7 +103,6 @@ async function showScoreCard(teeType) {
   let totalhcp = hcpSum + hcpSum2;
 
   try {
-    course = await loadCourse();
   } catch (error) {
     console.log(`ERROR: ${error}`);
   }
@@ -138,8 +147,6 @@ for (let i = 0; i < teeType.length; i++) {
   createButtonAddPlayers();
 }
 
-
-// boton agregar jugadores
 function createButtonAddPlayers() {
 document.getElementById('playerData').innerHTML = "<button id=\"addplayer\" onclick=\"addPlayers()\">Add Players</button>";
 let addplayer = document.getElementById("addplayer");
@@ -202,20 +209,83 @@ for (let i = 1; i <= 4; i++) {
   tbody.appendChild(row);
 }
 
-table.innerHTML = '<thead><th>Name</th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>Out</th><th>10</th><th>11</th><th>12</th><th>13</th><th>14</th><th>15</th><th>16</th><th>17</th><th>18</th><th>In</th><th>Total</th></thead>';
-table.appendChild(tbody);
-document.getElementById('scoreCard').appendChild(table);
+//Encabezados--------------------------------------------------
+const frontNine = document.getElementById('frontNine')
+let tr = document.createElement('tr')
+let th = document.createElement('th')
+th.textContent= 'Name'
+tr.appendChild(th)
+
+for(let i= 1; i<=9; i++){
+  let th = document.createElement('th')
+  th.textContent= i
+  tr.appendChild(th)
 }
 
 
+th = document.createElement('th')
+th.textContent= 'Out'
+tr.appendChild(th)
+for(let i= 10; i<=18; i++){
+  let th = document.createElement('th')
+  th.textContent= i
+  tr.appendChild(th)
+}
+
+th = document.createElement('th')
+th.textContent= 'In'
+tr.appendChild(th)
+
+th = document.createElement('th')
+th.textContent= 'Total'
+tr.appendChild(th)
+
+frontNine.appendChild(tr)
+//Fin de encabezados
 
 
-/*
-const scoreBody = document.getElementById('scoreCard');
-scoreBody.innerHTML = '';
-scoreBody.appendChild(tbody);
-*/
+for(let x=0; x<=3; x++){
 
+  let row = document.createElement('tr')
+  let td = document.createElement('td');
+  const nameInput = document.createElement('input');
+  nameInput.type = 'text';
+  nameInput.id = `playerName`;
+  nameInput.placeholder = `Player`;
+  td.appendChild(nameInput);
+  row.appendChild(td);
+
+  for(let i= 1; i<=9; i++){
+    let td = document.createElement('td')
+    const input = document.createElement('input');
+    input.type = 'number'; 
+    td.appendChild(input)
+    row.appendChild(td)
+  }
+
+  td = document.createElement('td')
+  row.appendChild(td)
+
+  for(let i= 1; i<=9; i++){
+    let td = document.createElement('td')
+    const input = document.createElement('input');
+    input.type = 'number'; 
+    td.appendChild(input)
+    row.appendChild(td)
+  }
+    td = document.createElement('td')
+    row.appendChild(td)
+    td = document.createElement('td')
+    row.appendChild(td)
+    frontNine.appendChild(row)
+  }
+
+
+
+/* table.innerHTML = '<thead><th>Name</th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>Out</th><th>10</th><th>11</th><th>12</th><th>13</th><th>14</th><th>15</th><th>16</th><th>17</th><th>18</th><th>In</th><th>Total</th></thead>';
+table.appendChild(tbody);
+document.getElementById('scoreCard').appendChild(table); */
+}
 
 
 
@@ -246,3 +316,8 @@ console.log(golfScore(4, 15));
 //notification toastr 
 toastr.success(`${playerName}, you are (L)PGA Tour material`);
 */
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  loadCourse()
+})
